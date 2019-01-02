30667163345eb901eb0c33ba037ee732ae14f7c6
guodong <gd@tongji.edu.cn>
2019-01-02 17:47:01 +0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import fpm_pb2 as fpm
 
 addr = ('127.0.0.1', 26200)
 s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(10)
 while True:
@@ -10,8 +11,13 @@ while True:
     print("new conn")
     data = b''
     while True:
-        data += s.recv(1000)
+        data += conn.recv(1000)
         m = fpm.Message()
         m.ParseFromString(data)
         print(m.type)
-    conn.close()
+        if m.type == fpm.Message.ADD_ROUTE:
+            print("add route")
+        elif m.type == fpm.Message.REMOVE_ROUTE:
+            print("remove route")
+        else:
+            conn.close()
